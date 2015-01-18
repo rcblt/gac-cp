5021ce44bd856bf788804cccf8c3db8c34f76501
rcblt <rcblt@users.noreply.github.com>
2015-01-18 13:05:04 +0100

    rcblt: created run_analysis.R
    -

---
+++ run_analysis.R
@@ -0,0 +1,7 @@
+## This script converts the source data in a tidy dataset as
+## requested by the Course Project of the Getting and Cleaning
+## Data course (getdata-010) on Coursera
+
+## Write a short comment describing this function
+##
+
